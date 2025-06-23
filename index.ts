aa3578b479fce52098df4e667edea706183d8c55
PatrickDev-it <github@patrickdev.it>
2025-06-23 10:20:13 +0200

    .

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import {spawn} from 'child_process';
 import { env, runtime } from '@/lib/utils';
 
 if(env.MODE === 'prod') {
-    spawn(`bun build ./src/server/socket.bun.ts --target=bun  --minify --sourcemap --outdir dist`, { stdio: 'inherit' }); 
+    spawn(`bun build "./src/server/socket.bun.ts" --target=bun  --minify --sourcemap --outdir dist`, { stdio: 'inherit' }); 
 } else {
    const { default: server } = require(`./src/server/socket.${runtime}.ts`);
 
